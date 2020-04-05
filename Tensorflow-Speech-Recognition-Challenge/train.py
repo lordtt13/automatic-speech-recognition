ad93ae54c93e94024eac05543f2c4593883d9cb1
lordtt13 <thakurtanmay72@yahoo.com>
2020-04-05 11:08:53 +0530

    Minor Restructres
    Signed-off-by: lordtt13 <thakurtanmay72@yahoo.com>

--- Tensorflow-Speech-Recognition-Challenge/train.py
+++ Tensorflow-Speech-Recognition-Challenge/train.py
@@ -7,14 +7,24 @@ Created on Sun Apr  5 09:21:47 2020
 """
 
 import os
+import pickle
 import librosa
 
 import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
+from model import get_model
+from tensorflow.compat.v1 import ConfigProto
 from sklearn.preprocessing import LabelEncoder
+from tensorflow.compat.v1 import InteractiveSession
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 
+
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config = config)
 
 train_audio_path = "train/audio"
 
@@ -41,3 +51,34 @@ all_wave = np.array(all_wave).reshape(-1,8000,1)
 
 x_tr, x_val, y_tr, y_val = train_test_split(np.array(all_wave), np.array(y), \
                                             stratify = y, test_size = 0.2, random_state = 42, shuffle = True)
+    
+model = get_model(labels)
+
+model.summary()
+
+model.compile(loss = 'sparse_categorical_crossentropy', optimizer = tf.keras.optimizers.Adam(lr = 0.001), \
+              metrics = ['accuracy'])
+    
+es = EarlyStopping(monitor = 'val_loss', mode = 'min', verbose = 1, patience = 10, min_delta = 0.0001) 
+mc = ModelCheckpoint('best_model.hdf5', monitor = 'val_acc', verbose = 1, save_best_only = True, mode = 'max')
+
+history = model.fit(x_tr, y_tr, epochs = 100, callbacks = [es,mc], \
+                    batch_size = 32, validation_data = (x_val, y_val))
+    
+# Sace Model
+model.save("asr.h5")
+
+# Save Datasets for future use
+pickle_out = open("wav.pickle","wb")
+pickle.dump(all_wave, pickle_out)
+pickle_out.close()
+
+pickle_out = open("labels_clases.pickle","wb")
+pickle.dump((y, classes), pickle_out)
+pickle_out.close()
+
+# Plot training metrics
+plt.plot(history.history['loss'], label = 'train') 
+plt.plot(history.history['val_loss'], label = 'test') 
+plt.legend() 
+plt.show()
