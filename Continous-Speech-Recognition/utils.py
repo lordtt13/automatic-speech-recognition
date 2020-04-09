da915806e88b8d772fd79b3b2d711445124c8a81
lordtt13 <thakurtanmay72@yahoo.com>
2020-04-09 10:32:36 +0530

    Generate sparse tuples func add
    Signed-off-by: lordtt13 <thakurtanmay72@yahoo.com>

--- Continous-Speech-Recognition/utils.py
+++ Continous-Speech-Recognition/utils.py
@@ -43,3 +43,24 @@ def convert_inputs_to_ctc_format(audio, fs, target_text, num_features):
 
     return train_inputs, targets, train_seq_len, original
 
+
+def sparse_tuple_from(sequences, dtype = np.int32):
+    """Create a sparse representention of x.
+    Args:
+        sequences: a list of lists of type dtype where each element is a sequence
+    Returns:
+        A tuple with (indices, values, shape)
+    """
+    
+    indices = []
+    values = []
+
+    for n, seq in enumerate(sequences):
+        indices.extend(zip([n] * len(seq), range(len(seq))))
+        values.extend(seq)
+
+    indices = np.asarray(indices, dtype = np.int64)
+    values = np.asarray(values, dtype = dtype)
+    shape = np.asarray([len(sequences), np.asarray(indices).max(0)[1] + 1], dtype=np.int64)
+
+    return indices, values, shape
